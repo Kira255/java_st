2031aaea2bc250e9459daa4ff0d7c0ba97072cf1
Kira <kirakulikova255@gmail.com>
2023-02-20 00:00:08 +0300

    class creation

--- dz/dz3/MergeSort.java
+++ dz/dz3/MergeSort.java
@@ -3,3 +3,9 @@ package dz.dz3;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+
+public class MergeSort {
+
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
+}
