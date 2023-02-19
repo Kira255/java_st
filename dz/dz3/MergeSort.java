6110e2f54fdc152ff68bfdfcf875678e6656df42
Kira <kirakulikova255@gmail.com>
2023-02-20 00:04:57 +0300

    mergesortinner

--- dz/dz3/MergeSort.java
+++ dz/dz3/MergeSort.java
@@ -46,4 +46,35 @@ public class MergeSort {
         int[] result = mergesortInner(buffer1, buffer2, 0, array1.length);
         return result;
     }
+
+    // Сортировка
+    public static int[] mergesortInner(int[] buffer1, int[] buffer2,
+            int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
+
+        // Уже отсортированый массив
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergesortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergesortInner(buffer1, buffer2, middle, endIndex);
+
+        // Слияние
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++]
+                    : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
 }
