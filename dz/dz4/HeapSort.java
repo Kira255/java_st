f0913a5b10f7680c5183c84cdce19c98b756f9d6
Kira <kirakulikova255@gmail.com>
2023-02-22 20:32:43 +0300

    create array

--- dz/dz4/HeapSort.java
+++ dz/dz4/HeapSort.java
@@ -5,4 +5,10 @@ import java.util.Random;
 public class HeapSort {
     private static Random random = new Random();
     private static Scanner input = new Scanner(System.in);
+
+    // Ввод числа
+    public static int[] createArray(int size) {
+        return new int[size];
+    }
+
 }
