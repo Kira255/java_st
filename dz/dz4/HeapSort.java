ed64c0ded1e81c05ebbd3bcffb90115142149695
Kira <kirakulikova255@gmail.com>
2023-02-22 20:36:41 +0300

    main

--- dz/dz4/HeapSort.java
+++ dz/dz4/HeapSort.java
@@ -6,6 +6,16 @@ public class HeapSort {
     private static Random random = new Random();
     private static Scanner input = new Scanner(System.in);
 
+    // Главный модуль
+    public static void main(String args[]) {
+        int len = getNumberByUser("Введите количество элементов: ");
+        int[] items = createArray(len);
+        fillArray(items, -100, 100);
+        System.out.println(Arrays.toString(items));
+        int[] result = heapSort(items, len);
+        System.out.println(Arrays.toString(result));
+    }
+
     // Ввод числа
     public static int[] createArray(int size) {
         return new int[size];
