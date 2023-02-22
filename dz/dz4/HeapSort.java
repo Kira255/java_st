d3e2d0c57fbaabfd8ce7960490be119ba036b696
Kira <kirakulikova255@gmail.com>
2023-02-22 20:31:51 +0300

    adding scanner and randomizer

--- dz/dz4/HeapSort.java
+++ dz/dz4/HeapSort.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class HeapSort {
-
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
 }
