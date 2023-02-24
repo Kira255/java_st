a9632d13de28cbe5a8b423d6d6a9e5834a948d53
Kira <kirakulikova255@gmail.com>
2023-02-24 23:10:31 +0300

    wavealg

--- dz/dz5/WaveAlg.java
+++ dz/dz5/WaveAlg.java
@@ -5,4 +5,32 @@ import java.util.List;
 
 public class WaveAlg {
 
+    int width;
+    int height;
+    int wall = 99;
+    int[][] map;
+    List<Point> wave = new ArrayList<Point>();
+
+    // волновой алгоритм
+    public WaveAlg(int width, int height) {
+        this.width = width;
+        this.height = height;
+        map = new int[width][height];
+
+        // инициализируем карту
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                map[i][j] = -1;
+            }
+        }
+        // заполнение границ карты препятствиями
+        for (int i = 0; i < width; i++) {
+            map[i][0] = wall;
+            map[width - 1][i] = wall;
+        }
+        for (int i = 0; i < height; i++) {
+            map[0][i] = wall;
+            map[i][height - 1] = wall;
+        }
+    }
 }
