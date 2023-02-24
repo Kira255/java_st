9c33583e215b4f3a7d3a0951ed3aa1a5e191318d
Kira <kirakulikova255@gmail.com>
2023-02-24 23:15:28 +0300

    main modul

--- dz/dz5/WaveAlg.java
+++ dz/dz5/WaveAlg.java
@@ -5,6 +5,42 @@ import java.util.List;
 
 public class WaveAlg {
 
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 0;
+        int nx = 0;
+        int ny = 0;
+
+        int height = 20;
+        int width = 20;
+        WaveAlg Path = new WaveAlg(width, height);
+
+        Path.traceOut();
+        System.out.println();
+
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.println("Введите x начала ");
+            x = scan.nextInt();
+            System.out.println("Введите y начала ");
+            y = scan.nextInt();
+            System.out.println("Введите x конца ");
+            nx = scan.nextInt();
+            System.out.println("Введите y конца ");
+            ny = scan.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println();
+        }
+
+        Path.findPath(x, y, nx, ny);
+
+        Path.traceOut();
+        System.out.println();
+
+        Path.waveOut();
+    }
+
     int width;
     int height;
     int wall = 99;
