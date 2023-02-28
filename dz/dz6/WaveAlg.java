16d288077dc5bc108b892e1df8cdf098233cf418
Kira <kirakulikova255@gmail.com>
2023-03-01 00:11:04 +0300

    data creation

--- dz/dz6/WaveAlg.java
+++ dz/dz6/WaveAlg.java
@@ -4,5 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WaveAlg {
-
+    int width;
+    int height;
+    int wall = 99;
+    int[][] map;
+    List<Point> wave = new ArrayList<Point>();
 }
