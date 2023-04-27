1f6cae8f311ab19886efaeae0e4a222d7e099b1c
Kira <kirakulikova255@gmail.com>
2023-04-27 10:04:00 +0300

    Визуал для покупателей

---
+++ ToyShop/Views/BuyersView.java
@@ -0,0 +1,21 @@
+package ToyShop.Views;
+
+import java.util.List;
+
+import ToyShop.Classes.Buyer;
+
+public class BuyersView {
+    List<Buyer> buyers;
+
+    public BuyersView(List<Buyer> buyers) {
+        this.buyers = buyers;
+    }
+
+    public void ShowTable() {
+        // Вывести в консоль таблицу игрушек
+        System.out.println("\nТаблица Покупатели\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        for (Buyer item : buyers) {
+            System.out.println(item.toString());
+        }
+    }
+}
