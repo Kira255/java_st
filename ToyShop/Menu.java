d5ec5072c1a2b897df0d395cc249b39afb94b512
Kira <kirakulikova255@gmail.com>
2023-04-27 13:07:18 +0300

    метод: присвоение отправленных призов

--- ToyShop/Menu.java
+++ ToyShop/Menu.java
@@ -31,24 +31,57 @@ public class Menu {
         }
         ReturnToPrevPos();
 
-        public boolean PrizeAddNew() {
+    public boolean PrizeAddNew() {
+        DrawMod drawingModel = new DrawMod();
+        if (drawingModel.PrizesToAwardAddNew())
+            return true;
+        return false;
+    }
+
+    public void PrizesAwardedShowAll() {
+        // разыгранные призы
+        DrawMod drawingModel = new DrawMod();
+        if (drawingModel.loadPrizesAwarded()) {
+            drawingModel.ShowTablePrizesAwarded();
+        }
+        ReturnToPrevPos();
+        showDrawingMenu();
+    }
+
+    public boolean PrizeSetAsAwarded() {
+        // показать все разыгранные призы
+        PrizesToAwardShowAll();
+        System.out.print("Введите id разыгранного приза, для смены статуса на Вручен: ");
+        try {
+            int curId = Integer.parseInt(sc.nextLine());
+
+            System.out.print("Введите дату, время вручения (Enter - текущая. пример: 06.07.2023 15:10): ");
+            String curValue = sc.nextLine();
+            LocalDateTime curDate;
+            if (curValue.equals("")) {
+                curDate = LocalDateTime.now();
+            } else {
+                curDate = LocalDateTime.parse(sc.nextLine(), formatter);
+            }
+
             DrawMod drawingModel = new DrawMod();
-            if (drawingModel.PrizesToAwardAddNew())
+            // загружаем список разыгранных призов
+            if (!drawingModel.loadPrizesToAward()) {
+                System.out.print("Ошибка при загрузке списка разыгранных призов!");
+                return false;
+            }
+            // находим выбранный приз
+            Prize curPrize = drawingModel.getPrizeToAwardById(curId);
+            // переводим приз в другую таблицу
+            if (drawingModel.PrizeToAwardSetAsAwarded(curPrize, curDate)) {
+
                 return true;
+            }
+        } catch (Exception ex) {
+            System.out.println("Ошибка при редактировании игрушки.\n" + ex.toString());
             return false;
         }
-
-        public void PrizesAwardedShowAll() {
-            // разыгранные призы
-            DrawMod drawingModel = new DrawMod();
-            if (drawingModel.loadPrizesAwarded()) {
-                drawingModel.ShowTablePrizesAwarded();
-            }
-            ReturnToPrevPos();
-            showDrawingMenu();
-        }
-    
-        
+        return false;
     }
 
     public Menu() {
