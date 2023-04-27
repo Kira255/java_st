0057e07228a0341b16e4d1f7b10c4c031b01ce10
Kira <kirakulikova255@gmail.com>
2023-04-27 12:52:08 +0300

    меню, создание конструктора

---
+++ ToyShop/Menu.java
@@ -0,0 +1,32 @@
+package ToyShop;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import ToyShop.Classes.Buyer;
+import ToyShop.Classes.Prize;
+import ToyShop.Classes.Toy;
+import ToyShop.Models.BuyerMod;
+import ToyShop.Models.DrawMod;
+import ToyShop.Models.ToyMod;
+import ToyShop.Views.BuyersView;
+import ToyShop.Views.ToysView;
+
+public class Menu {
+    private String prevPos;
+    private String choice;
+    private String newPos; // выбор пункта меню
+    private boolean ShowNewChoice;
+    private Scanner sc;
+    DateTimeFormatter formatter; // формат для даты и времени
+
+    public Menu() {
+        prevPos = "";
+        choice = "";
+        newPos = "";
+        formatter = DateTimeFormatter.ofPattern(
+                "dd.MM.yyyy HH:mm");
+    }
+}
